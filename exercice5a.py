a8d982a7dd5451e7a77d3a76a95b57ba52d0d948
Bineta Badiane <binetabadiane6@gmail.com>
2025-12-11 19:22:15 +0000

    creation une fonction qui permet d ajouter une tache

---
+++ exercice5a.py
@@ -0,0 +1,10 @@
+def ajouter_tache():
+    description = input("Description de la tâche : ")
+    statut = input("Statut (à faire / en cours / terminé) : ")
+
+    print("\nTâche ajoutée :")
+    print(" -", description)
+    print(" -", statut)
+
+# Appel de la fonction
+ajouter_tache()
