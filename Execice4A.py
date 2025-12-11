7957862444902073eb8eb07ac78f42c95a8231d2
moussacode <samouh269@gmail.com>
2025-12-11 20:34:28 +0000

    Creation des fichier exercice et v1 des exos

---
+++ Execice4A.py
@@ -0,0 +1,7 @@
+nombre = int(input("Saisissez un nombre entier: "))
+if nombre % 2 == 0:
+    print("Le nombre est pair.")
+else:
+    print("Le nombre est impair.")
+print("Fin du programme.")
+# Fin du programme.
---
+++ Exercice4B.py
@@ -0,0 +1,7 @@
+mot = input("Saisissez un mot: ")
+if mot == mot[::-1]:
+    print("Le mot est un palindrome.")
+else:
+    print("Le mot n'est pas un palindrome.")
+print("Fin du programme.")
+# Fin du programme.
